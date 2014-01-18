0e3f1233337212a86ad2fccc197cc6b4348c0648
Jaime Rump <jaimerump@trengine.com>
2014-01-18 17:31:43 +0000

    Added the edit posts functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,7 +11,7 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
-
+  
   def create
     @post = Post.new(params[:post].permit(:title, :text))
 
@@ -22,6 +22,20 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+  
+  def update
+    @post = Post.find(params[:id])
+  
+    if @post.update(params[:post].permit(:title, :text))
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+  
   private
     def post_params
       params.require(:post).permit(:title, :text)
