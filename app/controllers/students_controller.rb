80053fd8fba1406826f0857da370ff9b61f335c5
= <=>
2014-01-26 17:43:11 +0000

    Added the update student functionality, complete with placeholder email and Gravatar

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -31,4 +31,31 @@ class StudentsController < ApplicationController
   
   #Student Update actions
   
+  def edit
+    @student = Student.find(params[:id])
+  end
+  
+  def update
+    @student = Student.find(params[:id])
+  
+    #Check for email, add placeholder if missing
+    if( params[:student][:email].empty? )
+      params[:student][:email] = "placeholder@example.com"
+    end
+    
+    #Check for image, consult Gravatar if missing
+    if( params[:student][:image_url].empty? )
+      require 'digest/md5'
+      email_address = params[:student][:email].downcase
+      hash = Digest::MD5.hexdigest(email_address)
+      params[:student][:image_url] = "http://www.gravatar.com/avatar/#{hash}"
+    end
+    
+    if @student.update(params[:student].permit!)
+      redirect_to students_path
+    else
+      render 'edit'
+    end
+  end
+  
 end
