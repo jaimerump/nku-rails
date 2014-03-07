3efee9c414d178fb55471147068d3e2f7fb03bf4
Jaime Rump <rumpj2@nku.edu>
2014-03-07 22:06:08 +0000

    Create assignment model

---
+++ app/models/assignment.rb
@@ -0,0 +1,22 @@
+class Assignment < ActiveRecord::Base
+  
+  def percentage
+    ( score * 100 ) / total
+  end
+  
+  def self.average_percentage
+    # Gets average across all assignments
+    
+    # Get all assignments
+    assignments = all
+    
+    # Sum up percentages
+    percentage_sum = 0
+    assignments.each{ |a| percentage_sum += a.percentage }
+    
+    # Get average percentage
+    percentage_sum / assignments.size
+    
+  end
+  
+end
