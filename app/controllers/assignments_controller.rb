2330f09a73715dee98d695aeaaf12f339aa0f7c6
Jaime Rump <rumpj2@nku.edu>
2014-03-19 18:12:27 +0000

    Implement assignments#index page

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -3,9 +3,8 @@ class AssignmentsController < ApplicationController
   def index
     # Displays all assignments for the given student, or for all students
     @current_student = get_current_student
-    @selected_student = @current_student
-    @assignments = Assignment.all
-=begin
+    @students = Student.all
+
     if( @current_student.is_admin? )
       # Admin can see all
       if( params[:student_id] )
@@ -14,23 +13,15 @@ class AssignmentsController < ApplicationController
         @assignments = @selected_student.assignments
       else
         # Fetch all assignments
-        @assignments = Assignments.all
+        @assignments = Assignment.all
       end
       
     else 
       # Student can only see their grades
-      
-      if( !params[:student_id] || @current_student.id != params[:student_id] )
-        # They're trying to see grades that aren't theirs
-        redirect_to students_path, notice: "You can't see someone else's grades!"
-      else
-        # They're good, fetch their assignments
-        @selected_student = @current_student
-        @assignments = @current_student.assignments
-      end
+      @selected_student = @current_student
+      @assignments = @current_student.assignments
       
     end
-=end
     
   end
   
